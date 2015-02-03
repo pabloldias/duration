764e7b6bff8ad77a82a50feca05e48097d6726d4
Pablo Lima Dias <pabloidz@gmail.com>
2015-02-03 19:35:03 -0200

    Obtaining video duration

--- Duration.groovy
+++ Duration.groovy
@@ -2,35 +2,43 @@ import groovy.io.FileType
 import groovy.json.JsonOutput
 
 def files = []
-def dir = new File("my_dir")
+def dir = new File("C:\\Users\\Pablo\\Downloads\\cursos")
 
-def getMetadata(file) {
+def getDuration(file) {
+
     def out = new StringBuilder()
     def err = new StringBuilder()
+
+    // ffprobe can be found in http://www.ffmpeg.org
     def proc = ['ffprobe', '-i', file.path, '-show_format'].execute()
     proc.waitForProcessOutput(out, err)
-    out.toString()
+
+    def duration = out.toString().find(/duration=(\w+).(\w+)/).split('=')[1]
+    duration.toDouble()
 }
 
 def getExtension(file) {
+
     def extension = file.name.lastIndexOf('.').with {it != -1 ? file.name[it..<file.name.length()] : "-"}
     extension
 }
 
 def isVideo(file) {
+
     getExtension(file) in ['.mp4']
 }
 
 
 def addFile(file) {
+
     [
-        fileName: file.name,
-        duration: file.size(),
-        metadata: getMetadata(file),
+        file_name: file.name,
+        duration: getDuration(file)
     ]    
 }
 
 def writeJsonFile(files) {
+
     def json = JsonOutput.toJson(files)
 
     File lista = new File("lista.json")
@@ -39,8 +47,8 @@ def writeJsonFile(files) {
     }    
 }
 
-
 dir.eachFileRecurse (FileType.FILES) { file -> 
+
     if (isVideo(file)) {
         files << addFile(file) 
     }    
